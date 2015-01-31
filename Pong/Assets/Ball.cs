a340d6ca74e4ee4768d949b8096121f2fd640e7e
kstoryy <krzysiek2393@gmail.com>
2015-01-31 14:47:37 +0100

    Grywalna wersja

---
+++ Pong/Assets/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	float x,y;
+	// Use this for initialization
+	void Start () {
+		x = Random.Range (0, 2) == 0 ? -1 : 1;
+		y = Random.Range (0, 2) == 0 ? -1 : 1;
+		rigidbody.velocity = new Vector3 (Random.Range (5, 10) * x, Random.Range (5, 10) * y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Pong/Assets/Control.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float spd = 0.2f;
+		if(gameObject.name=="Pallet1")transform.Translate (0,Input.GetAxis("Horizontal") * spd,0);
+		if (gameObject.name == "Pallet2")transform.Translate (0, Input.GetAxis ("Vertical") * spd, 0);
+
+		                                                
+	}
+}
