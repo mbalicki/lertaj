59d5853590c8ddaa2cd11ccfc4f79b9c93d71fbf
kstoryy <krzysiek2393@gmail.com>
2015-01-31 16:31:04 +0100

    Wyświetlanie wyniku

--- Pong/Assets/Ball.cs
+++ Pong/Assets/Ball.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class Ball : MonoBehaviour {
-
+	int scorea, scoreb;
 	float x,y;
 	// Use this for initialization
 	void Start () {
+		scorea = 0;
+		scoreb = 0;
+		updateScore (scorea, scoreb);
 		x = Random.Range (0, 2) == 0 ? -1 : 1;
 		y = Random.Range (0, 2) == 0 ? -1 : 1;
 		rigidbody.velocity = new Vector3 (Random.Range (5, 10) * x, Random.Range (5, 10) * y, 0);
@@ -13,6 +16,30 @@ public class Ball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (transform.position.x < -13) {
+			scoreb++;
+			updateScore(scorea,scoreb);
+			
+			transform.position = new Vector3 (0, 1, 0);
+			rigidbody.velocity = new Vector3 (Random.Range (5, 10) * x, Random.Range (5, 10) * y, 0);
+		}
+		if (transform.position.x > 13) {
+
+			scorea++;
+			updateScore(scorea,scoreb);
+
+			transform.position = new Vector3 (0, 1, 0);
+			rigidbody.velocity = new Vector3 (Random.Range (5, 10) * x, Random.Range (5, 10) * y, 0);
+		}
+	}
+	void updateScore(int scorea,int scoreb)
+	{
+		GameObject obiekt = GameObject.Find ("Score1");
+		Text tekst = obiekt.GetComponent<Text> ();
+		tekst.text = scorea.ToString ();
+
+		obiekt = GameObject.Find ("Score2");
+		tekst = obiekt.GetComponent<Text> ();
+		tekst.text = scoreb.ToString ();
 	}
 }
